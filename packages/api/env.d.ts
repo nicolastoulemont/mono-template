bc0a5d7ee2d7e1b633f3baa1d2771b503b1c431d
ntoulemont <n.toulemont@gmail.com>
2021-07-14 09:36:46 +0200

    update api env types

--- packages/api/env.d.ts
+++ packages/api/env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     TS_NODE_PROJECT: string;
+    NODE_ENV: string;
     PORT: string;
     CLIENT_APP_URL: string;
     DATABASE_URL: string;
@@ -9,6 +10,9 @@ declare namespace NodeJS {
     DOMAIN_URL: string;
     EMAIL_USER: string;
     EMAIL_PASSWORD: string;
+    EMAIL_CONTACT_ADDRESS: string;
+    EMAIL_BRAND_NAME: string;
     TOKEN_SECRET: string;
+    S3_BUCKET_NAME: string;
   }
 }
